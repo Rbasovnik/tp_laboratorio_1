0a86f20de17cb162ba4e05b808fd122a16078b8d
Rbasovnik <47338120+Rbasovnik@users.noreply.github.com>
2019-11-06 23:49:43 -0300

    Add files via upload

---
+++ TrabajoPractico3_RodrigoBasovnik_1F/Employee.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <conio.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+Employee* employee_new(){
+
+    Employee* this = malloc(sizeof(Employee));
+
+    if (this != NULL){
+        this->id = 0;
+        strcpy(this->nombre, " ");
+        this->horasTrabajadas = 0;
+        this->sueldo = 0;
+    }
+    return this;
+}
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr, char* sueldoStr){
+    Employee* this;
+
+    if(idStr != NULL && nombreStr != NULL && horasTrabajadasStr != NULL && sueldoStr != NULL){
+        this = employee_new();
+            if(this != NULL){
+                if(employee_setId(this, atoi(idStr)) == 0 || employee_setNombre(this, nombreStr) == 0 || employee_setHorasTrabajadas(this, atoi(horasTrabajadasStr)) == 0 || employee_setSueldo(this, atoi(sueldoStr)) == 0){
+                    free(this);
+                    this = NULL;
+            }
+        }
+    }
+    return this;
+}
+
+void employee_delete();
+
+int employee_setId(Employee* this,int id){
+    int resultado = 0;
+
+        if(this != NULL && id > 0){
+            this->id;
+            resultado = 1;
+        }
+
+    return resultado;
+}
+
+int employee_getId(Employee* this,int* id){
+    if(this != NULL && id > 0){
+        *id = this-> id;
+    }
+}
+
+int employee_setNombre(Employee* this,char* nombre){
+    int result = 0;
+        if(this != NULL && nombre != '\0'){
+        this->nombre;
+        result = 1;
+        }
+    return result;
+}
+int employee_getNombre(Employee* this,char* nombre);
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas){
+    int result = 0;
+        if(this != NULL && horasTrabajadas > 0){
+        this->horasTrabajadas;
+        result = 1;
+        }
+    return result;
+}
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
+
+int employee_setSueldo(Employee* this,int sueldo){
+    int res = 0;
+        if(this != NULL && sueldo > 999){
+            this->sueldo;
+            res = 1;
+        }
+    return res;
+}
+int employee_getSueldo(Employee* this,int* sueldo);
+
+void printMenu(){
+    printf("********Menu de opciones********\n");
+    printf("1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).\n");
+    printf("2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).\n");
+    printf("3. Alta de empleado.\n");
+    printf("4. Modificar datos de empleado.\n");
+    printf("5. Baja de empleado.\n");
+    printf("6. Listar empleados\n");
+    printf("7. Ordenar empleados\n");
+    printf("8. Guardar los datos de los empleados en el archivo data.csv (modo texto).\n");
+    printf("9. Guardar los datos de los empleados en el archivo data.csv (modo binario).\n");
+    printf("10. Salir.\n");
+    printf("Ingrese la opcion deseada\n");
+}
---
+++ TrabajoPractico3_RodrigoBasovnik_1F/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).
+     3. Alta de empleado
+     4. Modificar datos de empleado
+     5. Baja de empleado
+     6. Listar empleados
+     7. Ordenar empleados
+     8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
+     9. Guardar los datos de los empleados en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+
+int main()
+{
+    int option = 0;
+    LinkedList* listaEmpleados = ll_newLinkedList();
+    do{
+        system("cls");
+        printMenu();
+        scanf("%d", option);
+        switch(option)
+        {
+            case 1:
+                printf("Opcion cargar los datos de los empleados desde el archivo data.csv (modo texto)\n");
+                controller_loadFromText("data.csv",listaEmpleados);
+                break;
+            case 2:
+                printf("Opcion cargar los datos de los empleados desde el archivo data.csv (modo Binario)\n");
+
+                break;
+            case 3:
+                printf("Opcion Alta Empleado.\n");
+
+                break;
+            case 4:
+                printf("Opcion Modificar Empleado.\n");
+
+                break;
+            case 5:
+                printf("Opcion Baja Empleado.\n");
+
+                break;
+            case 6:
+                printf("Opcion Listar Empleados.\n");
+
+                break;
+            case 7:
+                printf("Opcion Ordenar Empleados.\n");
+
+                break;
+            case 8:
+                printf("Opcion Guardar los datos de los empleados en el archivo data.csv (modo texto).\n");
+
+                break;
+            case 9:
+                printf("Opcion Guardar los datos de los empleados en el archivo data.csv (modo binario).\n");
+
+                break;
+            case 10:
+                printf("Opcion Salir\n");
+
+                break;
+            default:
+                printf("Opcion Incorrecta");
+                break;
+        }
+    }while(option != 10);
+    return 0;
+}
