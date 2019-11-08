3cd7e83eea2d834d57f95df7114b3a6cc27821de
Rbasovnik <47338120+Rbasovnik@users.noreply.github.com>
2019-11-08 01:08:10 -0300

    Add files via upload

--- TrabajoPractico3_RodrigoBasovnik_1F/parser.c
+++ TrabajoPractico3_RodrigoBasovnik_1F/parser.c
@@ -15,8 +15,29 @@
  */
 int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
 {
+    char input[4][30];
+        int cant,response = 0;
+        Employee* empleado;
+        if(pFile != NULL && pArrayListEmployee != NULL){
+            while(!feof(pFile)){
+                cant = fscanf(pFile,"%[^,],%[^,],%[^,],%[^\n],\n", input[0], input[1], input[2], input[3]);
+                empleado = employee_newParametros(input[0],input[1],input[2],input[3]);
+                if(empleado != NULL && employee_isValid(empleado)){
+                    ll_add(pArrayListEmployee,empleado );
+                    response = 1;
+                }
+                if(cant < 4){
+                    if(feof(pFile)){
+                        break;
+                    }else{
+                        response = 0;
+                        break;
+                    }
+                }
+            }
+        }
 
-    return 1;
+    return response;
 }
 
 /** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
@@ -28,6 +49,31 @@ int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
  */
 int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
 {
+    int respons = 0;
+    int cant = 0;
+    Employee* empleado;
+    if(pFile != NULL && pArrayListEmployee != NULL){
+        while(!feof(pFile)){
+            empleado = employee_new();
+            cant = fread(empleado,sizeof(Employee),1,pFile);
+            if(empleado != NULL){
+                if(employee_isValid(empleado) == 1){
+                    ll_add(pArrayListEmployee,empleado);
+                    response = 1;
+                }else{
+                    employee_delete(empleado);
+                }
+            }
+            if(cant < 1){
+                if(feof(pFile)){
+                break;
+                }else{
+                    respons = 0;
+                    break;
+                }
+            }
+        }
+    }
 
-    return 1;
+    return response;
 }
